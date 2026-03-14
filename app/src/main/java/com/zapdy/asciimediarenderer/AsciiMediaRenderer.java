10f795e8090196c03b908ed075e38ff03922a7c0
= <zapdyy@gmail.com>
2026-03-14 20:53:48 +0100

    feat: resize image to terminal size

--- app/src/main/java/com/zapdy/asciimediarenderer/AsciiMediaRenderer.java
+++ app/src/main/java/com/zapdy/asciimediarenderer/AsciiMediaRenderer.java
@@ -1,5 +1,7 @@
 package com.zapdy.asciimediarenderer;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class AsciiMediaRenderer {
@@ -8,6 +10,7 @@ public class AsciiMediaRenderer {
     private static final double BLUE_WEIGHT = 0.07;
 
     private static final char[] ASCII_CHARACTERS = " -.`-,:'_;~*\"\\/^i!rl+|I=)(t<j>f1}{vx?L7z][JcTnuysYkohF4eaV3205pbqdXPZUC69K#AwHmg8E%&S$DORNGQBMW@".toCharArray();
+
     private static char getCharacterFromBrightness(double brightness) {
         double brightnessPercentage = brightness / 255;
         int asciiCharIndex = (int) (brightnessPercentage * (ASCII_CHARACTERS.length - 1));
@@ -21,14 +24,27 @@ public class AsciiMediaRenderer {
 
         return (RED_WEIGHT * r) + (GREEN_WEIGHT * g) + (BLUE_WEIGHT * b);
     }
+
+    private static BufferedImage resizeImage(BufferedImage image, int width, int height) {
+        BufferedImage resizedImage = new BufferedImage(width, height, image.getType());
+        Graphics2D graphics2d = resizedImage.createGraphics();
+        graphics2d.setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BICUBIC
+        );
+        graphics2d.drawImage(image, 0, 0, width, height, null);
+        graphics2d.dispose();
+        return resizedImage;
+    }
     
-    public static void printAsciiImage(BufferedImage image) {
+    public static void printAsciiImage(BufferedImage image, int terminalColumns, int terminalRows) {
+        image = resizeImage(image, terminalColumns / 2, terminalRows);
         for (int h = 0; h < image.getHeight(); h++) {
             for (int w = 0; w < image.getWidth(); w++) {
                 double brightness = getBrightnessFromRGB(image.getRGB(w, h));
                 IO.print(getCharacterFromBrightness(brightness));
             }
-            IO.println();
+            IO.print("\n");
         }
     }
 }
--- app/src/main/java/com/zapdy/asciimediarenderer/Main.java
+++ app/src/main/java/com/zapdy/asciimediarenderer/Main.java
@@ -6,17 +6,30 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.jline.terminal.*;
+
 public class Main {
     public static void main(String[] args) {
+        Size size = new Size(170, 40);
+
+        Size detectedTerminalSize;
+		try {
+			detectedTerminalSize = TerminalBuilder.builder().system(true).build().getSize();
+            if (detectedTerminalSize.getColumns() != 0 && detectedTerminalSize.getRows() != 0) {
+                size = detectedTerminalSize;
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
         File file = new File(args[0]);
-        IO.println(file.getAbsolutePath());
     	try {
 			BufferedImage image = ImageIO.read(file);
             if (image == null) {
                 IO.println("No registered ImageReader was able to read stream");
                 return;
             }
-            AsciiMediaRenderer.printAsciiImage(image); 
+            AsciiMediaRenderer.printAsciiImage(image, size.getColumns(), size.getRows()); 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
